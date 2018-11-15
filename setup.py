b23a3d253024e92dd04b8ab70cc2aede55ad0f27
Simon Burton <simon@arrowtheory.com>
2018-11-15 15:40:00 +0000

    begin here

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='qupy',
+      version='0.1',
+      description='Quantum python',
+      author='Simon Burton',
+      author_email='simon@arrowtheory.com',
+      url='https://github.com/punkdit/qupy',
+      packages=['qupy',],
+)
+
+
