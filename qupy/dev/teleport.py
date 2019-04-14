7f1fe6b3aa19e271d2542b718f4a48136aed0e98
Simon Burton <simon@arrowtheory.com>
2019-04-14 13:43:22 +0100

    commit edits

--- qupy/dev/teleport.py
+++ qupy/dev/teleport.py
@@ -42,6 +42,14 @@ bell = ir2 * (bitvec(0, 0) + bitvec(1, 1))
 def test():
     assert CX == X.control()
 
+    assert CX == X.control(1, 0) # target, source
+    assert CX != X.control(0, 1) # target, source
+
+    assert CZ == Z.control(1, 0) # target, source
+    assert CZ == Z.control(0, 1) # target, source
+
+    #assert CZ * (H @ H) * CZ == SWAP
+
     assert (I@H) * CZ * (I@H) == CX 
     assert (I@H) * CX * (I@H) == CZ
     assert CX * (I@H) == (I@H) * CZ
@@ -91,44 +99,48 @@ def gate_teleport():
 
     #print(lhs.shortstr())
 
-    lhs = Z.control(2, 4, rank=6) * lhs 
-    lhs = Z.control(3, 4, rank=6) * lhs 
-    lhs = H.bounce(2, rank=6) * lhs
-    lhs = Z.control(2, 4, rank=6) * lhs 
+    lhs = X.control(2, 4, rank=6) * lhs 
+    #lhs = Z.control(3, 4, rank=6) * lhs 
+    lhs = X.control(2, 5, rank=6) * lhs 
 
+    lhs = Z.control(2, 0, rank=6) * lhs 
     lhs = Z.control(3, 0, rank=6) * lhs 
-    lhs = H.bounce(3, rank=6) * lhs
-    lhs = Z.control(2, 1, rank=6) * lhs 
     lhs = Z.control(3, 1, rank=6) * lhs 
 
     x = plus
-    x = zero
     lhs = (~x @ ~x @ I @ I @ ~x @ ~x) * lhs
     print(lhs.valence)
     print(lhs.shortstr())
 
-    print(CZ.valence)
-    print(CZ.shortstr())
+    print(CX.valence)
+    print(CX.shortstr())
+
 
 
 def search():
 
     CX = X.control(0, 1) # target, source
 
-    chi = (I @ CX @ I) * (bell @ bell) # looking to teleport a CX gate
-    
-    B = (H @ H) * CZ
+    assert CZ == Z.control(0, 1)
+    assert CZ == Z.control(1, 0)
+    #print(CZ.flat().v)
+
+    #print(X.control(0, 1).flat().v)
+    #print(X.control(1, 0).flat().v)
+
+    #chi = (I @ CX @ I) * (bell @ bell) # looking to teleport a CX gate
+    #B = (H @ H) * CZ
+
+    chi = (I @ CZ @ I) * (bell @ bell) # looking to teleport a CZ gate
+    B = CZ
 
     start = (B @ I @ I @ B) * (I @ chi @ I)
 
-    #print(lhs.shortstr())
-
-    op = CZ
-
-    #for ops in cross([(I, X, Z)]*6):
     for idxs in cross([(0, 1, 2)]*6):
 
         ops = [(I, X, Z)[i] for i in idxs]
+
+        # IXX XIX
     
         lhs = ops[0].control(2, 4, rank=6) * start 
         lhs = ops[1].control(3, 4, rank=6) * lhs 
@@ -138,8 +150,10 @@ def search():
         lhs = ops[4].control(3, 0, rank=6) * lhs 
         lhs = ops[5].control(3, 1, rank=6) * lhs 
 
+        lhs0 = lhs
+
         x = plus
-        #x = zero
+        #x = one
         lhs = (~x @ ~x @ I @ I @ ~x @ ~x) * lhs
         #print(lhs.valence)
         #print(lhs.valence, lhs.shortstr(), lhs[0,0,0,0])
@@ -150,16 +164,25 @@ def search():
 
         lhs /= r
 
-        if lhs == CX:
+        if lhs == CZ:
             print("FOUND "*10)
             print(lhs.shortstr())
             print(['IXZ'[i] for i in idxs])
 
+            break
 
-    print(lhs.valence)
-    print(lhs.shortstr())
-    print(CX.valence)
-    print(CX.shortstr())
+
+    if 0:
+        print(lhs0.shortstr())
+        op = (Z.control(0, 5) * (I @ plus @ plus @ plus @ plus @ I))
+        print(op.shortstr())
+    
+
+
+    #print(lhs.valence)
+    #print(lhs.shortstr())
+    #print(CX.valence)
+    #print(CX.shortstr())
 
 
 
