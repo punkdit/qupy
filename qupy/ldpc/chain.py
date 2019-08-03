e290588f1a3ca1d951876c8088342927c88cf189
Simon Burton <simon@arrowtheory.com>
2019-08-03 15:19:22 +0100

    commit edits

--- qupy/ldpc/chain.py
+++ qupy/ldpc/chain.py
@@ -87,11 +87,12 @@ class Chain(object):
     Hs[i].shape[1] == Hs[i+1].shape[0]
     """
 
-    def __init__(self, Hs, Ls=None, d=2, check=True):
+    def __init__(self, Hs, Ls=None, d=2, dual=None, check=True):
         self.d = d
         #self.Hs = [H for H in Hs if H.shape[0] or H.shape[1]]
         self.Hs = list(Hs)
         self.Ls = Ls # homology
+        self._dual = dual
         if check:
             self.check()
 
@@ -138,10 +139,42 @@ class Chain(object):
     def __len__(self):
         return len(self.Hs)
 
-    def dual(self):
+    def __eq__(self, other):
+        if self is other:
+            return True
+        assert 0, "not implemented"
+
+    def __ne__(self, other):
+        assert 0, "not implemented"
+
+    def to_zero(self):
         Hs = self.Hs
-        Hs = list(reversed([H.transpose() for H in Hs]))
-        return Chain(Hs, d=self.d)
+        Ds = []
+        chain = [] # _zero
+        n = len(Hs)
+        for i in range(n):
+            H = Hs[i]
+            chain.append(zeros2(0, 0))
+            Ds.append(zeros2(0, H.shape[0]))
+        Ds.append(zeros2(0, H.shape[1]))
+        chain = Chain(chain)
+        assert len(self) == len(chain)
+        f = Map(self, chain, Ds)
+        return f
+
+    def from_zero(self):
+        dual = self.get_dual()
+        f = dual.to_zero()
+        f = f.get_dual()
+        assert f.src is self
+        return f
+
+    def get_dual(self):
+        if self._dual is None:
+            Hs = self.Hs
+            Hs = list(reversed([H.transpose() for H in Hs]))
+            self._dual = Chain(Hs, d=self.d, dual=self)
+        return self._dual
 
     def dim(self, i):
         Hs = self.Hs
@@ -273,9 +306,13 @@ class Map(object):
     def __init__(self, src, tgt, Ds, check=True):
         assert isinstance(src, Chain)
         assert isinstance(tgt, Chain)
-        assert len(src) == len(tgt) == len(Ds)-1
+        assert len(src) == len(tgt)
+        assert len(src) == len(Ds)-1
+        assert src.d == tgt.d
+        self.d = src.d
         self.src = src
         self.tgt = tgt
+        self.shape = (tgt, src)
         Ds = list(Ds)
         self.Ds = Ds
         for i, D in enumerate(Ds):
@@ -283,8 +320,27 @@ class Map(object):
         if check:
             self.check()
 
+    def __len__(self):
+        return len(self.Ds)
+
     def __getitem__(self, idx):
         return self.Ds[idx]
+
+    def __add__(self, other):
+        assert len(self) == len(other)
+        assert self.shape == other.shape
+        n = len(self)
+        d = self.d
+        Ds = [(self[i]+other[i])%d for i in range(n)]
+        return Map(self.src, self.tgt, Ds)
+
+    def __sub__(self, other):
+        assert len(self) == len(other)
+        assert self.shape == other.shape
+        n = len(self)
+        d = self.d
+        Ds = [(self[i]-other[i])%d for i in range(n)]
+        return Map(self.src, self.tgt, Ds)
 
     def check(self):
         src, tgt = self.src, self.tgt
@@ -304,7 +360,7 @@ class Map(object):
 def pushout(a, b):
     assert isinstance(a, Map)
     assert isinstance(b, Map)
-    assert a.src is b.src
+    assert a.src == b.src
 
     src = a.src
     n = len(src)
@@ -326,6 +382,18 @@ def pushout(a, b):
     return amap, bmap, c
 
 
+def equalizer(a, b):
+    assert isinstance(a, Map)
+    assert isinstance(b, Map)
+    assert a.shape == b.shape
+    tgt, src = a.shape
+
+    ab = a-b
+    c = src.to_zero()
+    amap, bmap, chain = pushout(ab, c)
+    return amap, bmap, chain
+
+
 def test_repitition():
 
     H = parse(
@@ -338,7 +406,7 @@ def test_repitition():
     X = Chain([H])
     X.check()
 
-    XX = X.tensor(X.dual(), 1)
+    XX = X.tensor(X.get_dual(), 1)
     print(XX)
     print(shortstr(XX[0]))
 
@@ -457,7 +525,7 @@ def test_toric4():
     """)
 
     T1 = Chain([H])
-    T1t = T1.dual()
+    T1t = T1.get_dual()
 
     T2 = T1.tensor(T1t)
     assert len(T2)==2
@@ -486,7 +554,7 @@ def test_product():
         X1 = Chain([code.Hz])
         X2 = Chain([code.Hz])
     
-        XX = X1.tensor(X2.dual())
+        XX = X1.tensor(X2.get_dual())
         #XX = X1.tensor(X2)
     
         print(len(XX.Hs))
--- qupy/ldpc/glue.py
+++ qupy/ldpc/glue.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
 import numpy
+import random
 
 from qupy.ldpc.solve import pushout, array2, parse, dot2, compose2, eq2, zeros2
+from qupy.ldpc.solve import rand2, find_kernel, image
 from qupy.ldpc import solve
 from qupy.ldpc import css
 from qupy.ldpc.chain import Chain, Map
 from qupy.ldpc import chain
+from qupy.argv import argv
 
 
 def test():
@@ -73,7 +76,7 @@ def test():
     H = code.Hx
     #print(H)
     d = H.shape[1]
-    for v in solve.image(H.transpose()):
+    for v in image(H.transpose()):
         v = (u+v)%2
         d = min(d, v.sum()) or d
         if v.sum()==4:
@@ -81,7 +84,7 @@ def test():
     print("distance:", d)
 
 
-def test_color():
+def test_selfdual():
 
     HxA = array2([[1,1,1,1]])
     HzA = array2([[1,1,1,1]])
@@ -118,9 +121,212 @@ def test_color():
     #print(code)
 
 
-if __name__ == "__main__":
-    #test()
-    test_color()
+def test_colimit():
 
+    n = 4
+    m = n-1
+    H = zeros2(m, n)
+    for i in range(m):
+        H[i, i] = 1
+        H[i, i+1] = 1
+
+    A = Chain([H])
+    B = Chain([zeros2(0, 0)])
+    C = Chain([array2([[1]])])
+
+    CAm = zeros2(m, 1)
+    CAm[0, 0] = 1
+    CAm[m-1, 0] = 1
+    CAn = zeros2(n, 1)
+    CAn[0, 0] = 1
+    CAn[n-1, 0] = 1
+    CA = Map(C, A, [CAm, CAn])
+
+    CBm = zeros2(0, 1)
+    CBn = zeros2(0, 1)
+    CB = Map(C, B, [CBm, CBn])
+
+    AD, BD, D = chain.pushout(CA, CB)
+    assert eq2(D[0], array2([[1,1,1], [0,1,1]])) # glue two checks at a bit
+
+    # --------------------------
+
+    A = Chain([H.transpose()])
+    B = Chain([zeros2(0, 0)])
+    C = Chain([zeros2(1, 0)])
+
+    CAn = zeros2(n, 1)
+    CAn[0, 0] = 1
+    CAn[n-1, 0] = 1
+    CAm = zeros2(m, 0)
+    CA = Map(C, A, [CAn, CAm])
+
+    CBn = zeros2(0, 1)
+    CBm = zeros2(0, 0)
+    CB = Map(C, B, [CBn, CBm])
+
+    AD, BD, D = chain.pushout(CA, CB)
+    D = D[0]
+    #print(D)
+    assert eq2(D, array2([[1,0,1], [1,1,0], [0,1,1]])) # glue two bits
+
+
+def test_equalizer():
+
+    n = 4
+    m = n-1
+    H = zeros2(m, n)
+    for i in range(m):
+        H[i, i] = 1
+        H[i, i+1] = 1
+
+    A = Chain([H])
+    C = Chain([array2([[1]])])
+
+    fm = zeros2(m, 1)
+    fm[m-1, 0] = 1
+    fn = zeros2(n, 1)
+    fn[n-1, 0] = 1
+    f = Map(C, A, [fm, fn])
+
+    gm = zeros2(m, 1)
+    gm[0, 0] = 1
+    gn = zeros2(n, 1)
+    gn[0, 0] = 1
+    g = Map(C, A, [gm, gn])
+
+    AD, BD, D = chain.equalizer(f, g)
+    assert eq2(D[0], array2([[1,1,1], [0,1,1]])) # glue two checks at a bit
+
+    # --------------------------
+
+    A = Chain([H.transpose()])
+    C = Chain([zeros2(1, 0)])
+
+    fn = zeros2(n, 1)
+    fn[0, 0] = 1
+    fm = zeros2(m, 0)
+    f = Map(C, A, [fn, fm])
+
+    gn = zeros2(n, 1)
+    gn[n-1, 0] = 1
+    gm = zeros2(m, 0)
+    g = Map(C, A, [gn, gm])
+
+    AD, BD, D = chain.equalizer(f, g)
+    D = D[0]
+    #print(D)
+    assert eq2(D, array2([[1,0,1], [1,1,0], [0,1,1]])) # glue two bits
+
+
+def wenum(H):
+    m, n = H.shape
+    K = find_kernel(H)
+    #print(H)
+    #print(K)
+    w = dict((i, []) for i in range(n+1))
+    for v in image(K.transpose()):
+        assert dot2(H, v).sum() == 0
+        w[v.sum()].append(v)
+    w = [w[i] for i in range(n+1)]
+    return w
+
+
+def glue2(H1, H2, i1, i2):
+
+    m1, n1 = H1.shape
+    m2, n2 = H2.shape
+
+    A1 = Chain([H1])
+    A2 = Chain([H2])
+    C  = Chain([array2([[1]])])
+
+    C1n = zeros2(n1, 1)
+    C1n[i1, 0] = 1
+    C1m = dot2(H1, C1n)
+    C1 = Map(C, A1, [C1m, C1n])
+
+    C2n = zeros2(n2, 1)
+    C2n[i2, 0] = 1
+    C2m = dot2(H2, C2n)
+    C2 = Map(C, A2, [C2m, C2n])
+
+    AD, BD, D = chain.pushout(C1, C2)
+
+    H = D[0]
+    #print(H.shape)
+    #print(H)
+    return H
+
+
+def glue1(H, i1, i2):
+
+    m, n = H.shape
+
+    A = Chain([H])
+    C  = Chain([array2([[1]])])
+
+    fn = zeros2(n, 1)
+    fn[i1, 0] = 1
+    fm = dot2(H, fn)
+    f = Map(C, A, [fm, fn])
+
+    gn = zeros2(n, 1)
+    gn[i2, 0] = 1
+    gm = dot2(H, gn)
+    g = Map(C, A, [gm, gn])
+
+    _, _, D = chain.equalizer(f, g)
+
+    H = D[0]
+    #print(H.shape)
+    #print(H)
+    return H
+
+
+
+def test_glue():
+
+    m, n = 7, 10
+    m = argv.get("m", 7)
+    n = argv.get("n", 10)
+
+    H = rand2(m, n)
+    print(H)
+    w = wenum(H)
+    print("wenum:", [len(wi) for wi in w])
+    v = w[4][0]
+    print("v:", v)
+
+    i0 = argv.i0
+    i1 = argv.i1
+    H1 = glue1(H, i0, i1)
+    print(H1)
+
+    w = wenum(H1)
+    print([len(wi) for wi in w])
+    #for v in w[4]:
+    #    print(v)
+
+
+
+
+    
+
+
+if __name__ == "__main__":
+
+    _seed = argv.get("seed")
+    if _seed is not None:
+        numpy.random.seed(_seed)
+        random.seed(_seed)
+
+    #test()
+    #test_selfdual()
+    test_colimit()
+    test_equalizer()
+    test_glue()
+
+    print("OK")
 
 
